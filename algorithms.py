93070ba75d4b93268fbb22d08c5f12426baac5dc
aj-staton <ajstaton0@gmail.com>
2019-10-01 15:02:39 -0400

    the beginning

---
+++ algorithms.py
@@ -0,0 +1,22 @@
+'''########################
+This file serves to store common algorithims that any Computing student
+should know.
+
+None of these algorithms are my own thought.
+
+Written by: Austin Staton 
+Date: October 1st, 2019
+####################################################################'''
+import numpy as math
+
+############
+# Sorting
+############
+def InsertionSort(array):
+    
+
+def SelectionSort(array):
+
+############
+# Searching
+############
