608ae3f4b043a1be03dae95c3d279bddaf0777b7
aj-staton <ajstaton0@gmail.com>
2019-10-31 17:42:24 -0400

    framework

---
+++ karatsuba/austinstaton_project3.cpp
@@ -0,0 +1,38 @@
+/*##############################################################
+Project 3 for CSCE350 multiplies large numbers (numbers that
+struggle to fit in an integer, long, or other type). I will do
+this by breaking down the large number into thier own elements
+in a vector.
+
+Example: 2,334,345,532,389 * 89,794,289,298,000
+
+This file will solve this problem in two ways:
+ 1) Brute force. This is the highly intuitive
+    approach that many people learn in grade school.
+    	1234
+       *  21
+	1234     This algorithm takes n^2 time.
+     + 24680
+       25914
+ 2) The Karatsube Algorithm. This algorithm is much more
+    efficient, taking n^~1.58 time.
+
+Copyright: Austin Staton
+Date: October 31st, 2019
+
+I will be using resources from cplusplus.com
+##############################################################*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main(int argc, char *argv[]) {
+
+  return 0;
+}
